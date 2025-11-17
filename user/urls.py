b80349d7c273febf8b2733358787537456a0bac1
unknown <kononovb777@gmail.com>
2025-11-17 16:43:24 +0200

    Solution#3

--- user/urls.py
+++ user/urls.py
@@ -3,8 +3,9 @@ from user.views import CreateUserView, LoginUserView, ManageUserView
 from rest_framework.authtoken import views
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="register"),
-    path("login/", LoginUserView.as_view(), name="get_token"),
-    path("me/", ManageUserView.as_view(), name="me"),
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
+
 app_name = "user"
